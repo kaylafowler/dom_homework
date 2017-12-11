933d409d62cf218dcafe8cc457deaea0cceb4f48
SwayGatsby <kayla.fowler@hotmail.com>
2017-12-11 21:30:23 +0000

    refactored code to encapsulate creation of list items

--- public/app.js
+++ public/app.js
@@ -2,36 +2,37 @@ var app = function() {
   addCat("Peter","Dreamies","http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
 };
 
-const addCat = function(name, food, photoUrl) {
-  //1. Create the ul element and add the css class
-  // Create a node on the DOM with the name (e.g. 'div', 'span') of 'ul'
-  const unorderedListOfCats = document.createElement("ul");
-
-  // Add a class attribute to that node, with the value 'cat'
-  unorderedListOfCats.classList.add("cat")
-
-  //2. Create the li elements
-  // Create a node on the DOM with the name (e.g. 'div', 'span') of 'li'
+const createNameListItem = function(name){
   const nameListItem = document.createElement("li");
+  nameListItem.innerText = `Name: ${name}`;
+  return nameListItem;
+};
+
+const createFavouriteFoodListItem = function(food){
   const favouriteFoodListItem = document.createElement("li");
+  favouriteFoodListItem.innerText = `Favourite food: ${food}`;
+  return favouriteFoodListItem;
+};
+
+const createPhotoListItem = function(photoUrl){
   const photoListItem = document.createElement("li");
   const photoImage = document.createElement("img");
   photoImage.setAttribute('width', 500);
   photoImage.setAttribute("src", `${photoUrl}`);
-
-  // Give it the innerText of 'blah blah blah'
-  // i.e. <foo>bar</foo>  has the innerText of 'bar'
-  ////blockquote.innerText = "blah blah blah";
-  nameListItem.innerText = `Name: ${name}`;
-  favouriteFoodListItem.innerText = `Favourite food: ${food}`;
-
   photoListItem.appendChild(photoImage);
-  unorderedListOfCats.appendChild(nameListItem);
-  unorderedListOfCats.appendChild(favouriteFoodListItem);
-  unorderedListOfCats.appendChild(photoListItem);
+  return photoListItem;
+};
+
+const addCat = function(name, food, photoUrl) {
+  const unorderedListOfCatItems = document.createElement("ul");
+  unorderedListOfCatItems.classList.add("cat")
+
+  unorderedListOfCatItems.appendChild(createNameListItem(name));
+  unorderedListOfCatItems.appendChild(createFavouriteFoodListItem(food));
+  unorderedListOfCatItems.appendChild(createPhotoListItem(photoUrl));
 
   const cats = document.querySelector("#cats");
-  cats.appendChild(unorderedListOfCats);
+  cats.appendChild(unorderedListOfCatItems);
 }
 
 document.addEventListener('DOMContentLoaded', app);
